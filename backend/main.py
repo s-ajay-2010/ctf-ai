ebafefbff6b0d87d0fa2ad21d360757671b93a4f
s-ajay-2010 <ajaykrishnaasasikumar2020@gmail.com>
2026-04-04 22:54:00 +0530

    login auth added

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,103 @@
+from jose.exceptions import JWTError
+from dotenv import load_dotenv
+import os
 from fastapi import FastAPI as fahh
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic import BaseModel as bm
+from passlib.context import CryptContext as cc
+from jose import jwt
+from datetime import datetime, timedelta, timezone
 
-app = fahh()
+load_dotenv()
+SECRET_KEY =os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY not in environment")
+
+security = HTTPBearer()
+
+ALGORITHM = "HS256"
+
+
+pwd_context = cc(schemes=["bcrypt"], deprecated="auto")
+app=fahh()
+
+users=[]
+login_attempts= {}
+
+class User(bm):
+    username:str
+    password:str
+
+def hash_password(password: str):
+    # print("Length:", len(password))
+    return pwd_context.hash(password[:72])
+
+def verify_password(plain, hashed):
+    return pwd_context.verify(plain[:72], hashed)
+
+
 
 @app.get("/")
 def home():
     return{"message": "CTF backend is ready!!"}
+
+
+
+@app.post("/register")
+def register(user: User):
+    for u in users:
+        if u["username"] == user.username:
+            raise HTTPException(status_code=400, detail="User already exists!!")
+    
+    users.append({
+        "username": user.username,
+        "password": hash_password(user.password)
+    })
+
+    return {"msg": "User registered successfully"}
+
+@app.post("/login")
+def login(user: User):
+    attempts = login_attempts.get(user.username, 0)
+    if attempts>=5:
+        raise HTTPException(status_code=429, detail="Too many attempts")
+    for u in users:
+        if u["username"] == user.username and verify_password(user.password, u["password"]):
+            login_attempts[user.username] =0
+            token = create_token({"sub": user.username, "type": "access"})
+            return {"access_token": token, "token_type": "bearer"}
+    
+    login_attempts[user.username] = attempts+1        
+    raise HTTPException(status_code=401, detail="Invaild username or password.")
+
+
+def create_token(data: dict):
+    to_encode = data.copy()
+    now =datetime.now(timezone.utc)
+    expire = now + timedelta(hours=1)
+    to_encode.update({"exp": expire, "iat": now})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user = payload.get("sub")
+
+        if user is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        if user not in [u["username"] for u in users]:
+            raise HTTPException(status_code=401, detail="User no longer exists")
+        
+        return user
+    
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+@app.get("/protected")
+def protected(user: str = Depends(get_current_user)):
+    return{"message": f"Hello {user}, you are authenticated"}
+
+
