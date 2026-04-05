c71e34b869e67427e51a3eb2d66d6156f26b007f
s-ajay-2010 <ajaykrishnaasasikumar2020@gmail.com>
2026-04-05 18:33:42 +0530

    dashboard ui

--- frontend/script.js
+++ frontend/script.js
@@ -20,5 +20,83 @@ async function login(){
         document.getElementById("msg").innerText = data.detail;
     }
 
+    const container = document.getElementById("challenges");
+    container.innerHTML = "";
+
+    data.forEach(c => {
+        container.innerHTML += `
+         <div>
+             <h3>${c.title} (${c.points} pts)</h3>
+             <p>${c.description}</p>
+             <p>Status: ${c.solved ? "Solved!!" : "Not solved bruh (-_-)"}</p>
+
+             <input id="flag-${c.id}" placeholder="Enter flag">{/*</input>*/}
+             <button onclick="submitFlag(${c.id})">Submit</button>
+             <button onclick="gethints(${c.id})">Hints</button>
+
+             <p id="result-${c.id}"></p>
+             <hr>
+         </div>
+        `;
+    });
+
 }
+
+
+async function submitFlag(id) {
+    const token = localStorage.getItem("token");
+    const flag = document. getElementById(`flag-${id}`).value;
+
+    const res = await fetch(`${API}/submit`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Aithorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            challenge_id: id,
+            flag: flag
+        })
+    });
+
+    const data = await res.json();
+    document.getElementById(`result-${id}`).innerText = data.message;
+    loadChallenges();
+}
+
+
+async function getHints(id){
+    const token = localStorage.getItem("token");
+    const res = await fetch(`${API}/hints/${id}`,{
+        headers: {
+            "Authorization": `Bearer ${token}`
+        }
+    });
+
+    const data = await res.json();
+    alert(data.hints.join("\n"));
+}
+
+async function loadScoreboard(){
+    const token = localStorage.getItem("token");
+    const res = await fetch(`${API}/scoreboard`,{
+        headers:{
+            "Authorization": `Bearer ${token}`
+        }
+    });
+
+    const data = await res.json();
+    const container = document.getElementById("scoreboard");
+    container.innerHTML = "";
+
+    data.forEach(u=>{
+        container.innerHTML += `<p>&{u.user}: ${u.score}</p>`;
+    });
+}
+
+
+if(window.location.pathname.includes("dashboard.html")){
+    loadChallenges();
+}
+
 
