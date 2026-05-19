261ac7384fa78dcb170fd0fb125fae07d1284a90
s-ajay-2010 <ajaykrishnaasasikumar2020@gmail.com>
2026-05-19 11:20:15 +0530

    fight vercellllgit pushgit pushgit pushgit pushgit pushgit push!

--- api/main.py
+++ api/main.py
@@ -17,8 +17,8 @@ init_db()
 
 load_dotenv()
 SECRET_KEY =os.getenv("SECRET_KEY")
-if not SECRET_KEY:
-    raise ValueError("SECRET_KEY not in environment")
+# if not SECRET_KEY:
+#     raise ValueError("SECRET_KEY not in environment")
 
 security = HTTPBearer()
 
