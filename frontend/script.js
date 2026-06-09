7ccc95c9d9b0f28bdcfeb079324335bb17f1ec78
s-ajay-2010 <ajaykrishnaasasikumar2020@gmail.com>
2026-06-09 20:12:37 +0530

    Add features and fix bugs

--- frontend/script.js
+++ frontend/script.js
@@ -91,6 +91,11 @@ async function getHints(id){
 
     const data = await res.json();
     const container = document.getElementById(`hints-${id}`);
+    if(data.hints.length === 0){
+        container.innerHTML = "<p>No hints available</p>"
+        showToast("No hints found in db:(");
+        return;
+    }
     container.innerHTML = data.hints.map(h => `<p>
         Hint(${h.cost} pts):
         <span data-hint="${encodeURIComponent(h.hint)}" data-cost="${h.cost}" onclick="unlockHint(this)" style="cursor:pointer; color:#38bdf8;">
@@ -192,7 +197,7 @@ async function loadChallenges(){
     }
 
     if(category){
-        filtered = filtered.filter(c => c.category === category);
+        filtered = filtered.filter(c => c.category === category || c.category === "Both");
     }
     if(difficulty){
         filtered = filtered.filter(c => c.difficulty === difficulty);
@@ -267,6 +272,10 @@ async function createChallenge(){
     const result = await res.json();
     document.getElementById("msg").innerText = result.message;
 
+    if(!res.ok){
+        showToast(result.detail || "Challenge creation failed", "error")
+    }
+
     loadAdminChallenges();
 }
 
@@ -307,11 +316,17 @@ async function deleteChallenge(id){
 
     const token = localStorage.getItem("token");
 
-    await fetch(`${API}/admin/delete/${id}`, {
+    const res = await fetch(`${API}/admin/delete/${id}`, {
         method: "DELETE",
         headers: {"Authorization": `Bearer ${token}`}
     });
 
+    const result = await res.json();
+
+    if(!res.ok){
+        showToast(result.detail || "Challenge deletion failed", "error");
+        return;
+    }
     loadAdminChallenges();
 }
 
@@ -453,7 +468,7 @@ function editChallenge(id, title, description, flag, points, category, difficult
     })
     .then(data => {
         console.log("SERVER RESPONSE:", data);
-        alert(data.message || data.detail);
+        showToast(data.message || data.detail);
         location.reload();
     });
 }
